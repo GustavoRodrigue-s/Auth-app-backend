6c4b177c19548b99d4b8426ccae2f2061e485648
GustavoRodrigue-s <gustavo.fabiano.2010@hotmail.com>
2024-11-13 00:09:04 -0300

    feat: add migrations

---
+++ prisma/migrations/20241113030846_create_users_table/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
